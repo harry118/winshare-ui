7304dec587f6f11c16f0bb34c31c57cb45c71307
winshare_shu.gang <shugang@winshare.com.cn>
2022-09-01 10:47:40 +0800

    sg 测试

--- packages/order-list/src/demo/demo.tsx
+++ packages/order-list/src/demo/demo.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import OrderList from '@winshare/order-list';
+// import OrderList from '@winshare/order-list';
 
 const Demo: React.FC = () => {
-  return <OrderList detailUrl={'http://xxx.xxx.com'} />;
+  // return <OrderList detailUrl={'http://xxx.xxx.com'} />;
+  return null;
 };
 export default Demo;
