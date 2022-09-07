68889b6573c3a701c110d21f94fb6c78b5428bd2
junxuan.gxw <junxuan.gxw@dtyunxi.com>
2022-09-07 15:25:45 +0800

    feat: add config

--- packages/good-master-data/src/DataList/index.tsx
+++ packages/good-master-data/src/DataList/index.tsx
@@ -3,6 +3,7 @@ import { RightOutlined, LeftOutlined, CaretLeftFilled, CaretUpFilled } from '@an
 import { Table, Col, Divider } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { useHistory } from 'react-router-dom';
+// import { History } from 'umi';
 import IconFont from '../components/IconFont';
 import { dataList } from './constants';
 import styles from './index.module.less';
