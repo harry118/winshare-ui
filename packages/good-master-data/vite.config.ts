106dfc4f7d5798bccbb223e43295f43a4b1dc075
junxuan.gxw <junxuan.gxw@dtyunxi.com>
2022-09-07 15:18:17 +0800

    feat: add config

--- packages/good-master-data/vite.config.ts
+++ packages/good-master-data/vite.config.ts
@@ -26,12 +26,14 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['react', 'react-dom', 'antd'],
+      external: ['react', 'react-dom', 'antd', '@ant-design/pro-components'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          antd: 'antd',
+          ProComponents: 'ProComponents',
         },
       },
     },
